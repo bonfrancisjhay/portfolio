fe8655bfe7f83e5735dce0c447bbc1b4a53aac6d
Francis Bon <bonfrancisjhay@gmail.com>
2026-03-01 00:24:35 +0800

    fix cors headers

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,12 +4,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ChatController;
 
-Route::post('/chat', [ChatController::class, 'chat']);
-
-// Temporary debug route
-Route::get('/models', function () {
-    $apiKey = env('GEMINI_API_KEY');
-    $response = \Illuminate\Support\Facades\Http::withOptions(['verify' => false])
-        ->get('https://generativelanguage.googleapis.com/v1beta/models?key=' . $apiKey);
-    return $response->json();
+Route::options('/chat', function() {
+    return response()->json([], 200)
+        ->header('Access-Control-Allow-Origin', '*')
+        ->header('Access-Control-Allow-Methods', 'POST, OPTIONS')
+        ->header('Access-Control-Allow-Headers', 'Content-Type, Accept');
 });
+
+Route::post('/chat', [ChatController::class, 'chat']);
