9f4bee5d1e5912975016d544a5e2148a629bd92f
Francis Bon <bonfrancisjhay@gmail.com>
2026-03-01 00:54:05 +0800

    fix api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,11 +4,4 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ChatController;
 
-Route::options('/chat', function() {
-    return response()->json([], 200)
-        ->header('Access-Control-Allow-Origin', '*')
-        ->header('Access-Control-Allow-Methods', 'POST, OPTIONS')
-        ->header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-});
-
 Route::post('/chat', [ChatController::class, 'chat']);
