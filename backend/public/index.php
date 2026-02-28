4690a26240ff7c49bd36c7731bbf6d66c8542309
Francis Bon <bonfrancisjhay@gmail.com>
2026-03-01 00:59:30 +0800

    remove index

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,16 +1,5 @@
 <?php
 
-// Handle CORS
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Accept, Authorization');
-
-// Handle preflight OPTIONS request
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    exit();
-}
-
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
